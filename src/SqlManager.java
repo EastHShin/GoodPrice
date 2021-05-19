bb7a9280bafa89444eb1d898add3b93cdf012b1e
donghyun <ilovekdh1208@ajou.ac.kr>
2021-05-19 12:25:23 +0900

    add 'if not exists' when create table

--- src/SqlManager.java
+++ src/SqlManager.java
@@ -31,21 +31,21 @@ public class SqlManager {
 		Statement st = conn.createStatement();
 		ResultSet rs;
 		
-		String market = "CREATE TABLE Market (\r\n" + 
+		String market = "CREATE TABLE if not exists Market (\r\n" + 
 				"  marketName varchar(20),marketRate float, stKind varchar(20), city varchar(20),\r\n" + 
 				"primary key(marketName), foreign key(stKind) references Item(stKind)\r\n);";
-		String sell = "CREATE TABLE Sell (\r\n" + 
+		String sell = "CREATE TABLE if not exists Sell (\r\n" + 
 				"  stKind varchar, marketName varchar, itemRate float, month int, unit int, monAvgPrice int,\r\n" + 
 				"primary key(stKind,marketName),\r\n" + 
 				"foreign key(stKind) references Item(stKind),\r\n" + 
 				"foreign key(marketName) references Market(marketName)\r\n" + 
 				");";
-		String item = "CREATE TABLE Item (\r\n" + 
+		String item = "CREATE TABLE if not exists Item (\r\n" + 
 				"  stKind varchar(20), stItem varchar(20), stItemCode int, primary key(stKind)\r\n" + 
 				");";
-		String account = "CREATE TABLE Account ( Id_No int, user_Id varchar(20), password varchar(10), name varchar(10), email varchar(15),\r\n" + 
+		String account = "CREATE TABLE if not exists Account ( Id_No int, user_Id varchar(20), password varchar(10), name varchar(10), email varchar(15),\r\n" + 
 				"primary key(Id_No)\r\n);";
-		String comment = "CREATE TABLE Comment (\r\n" + 
+		String comment = "CREATE TABLE if not exists Comment (\r\n" + 
 				"  user_Id varchar(20), marketName varchar(20), Id_No int, comment varchar(50),\r\n" + 
 				"primary key(Id_No, marketName),\r\n" + 
 				"foreign key(marketName) references Market(marketName),\r\n" + 
